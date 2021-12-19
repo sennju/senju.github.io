3d81bccad675b1218d760b88add4e264e21795b8
sennju <95702847+sennju@users.noreply.github.com>
2021-12-19 14:21:51 +0530

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-
-    <header>
-        <nav class="navbar-r">
-            <a href="index.html" class="logo-r">SENJU</a>
-            <a href="index.html" class="nav-r">Home</a>
-            <a href="about.html" class="nav-r">About</a>
-            <a href="contact.html" class="nav-r">Contact</a>
-        </nav>
-    </header>
